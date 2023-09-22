74cc5d41318da8bf430e8098f642b0991ad90274
Guilherme Noé <guilherme.snoe@gmail.com>
2023-09-22 02:21:11 -0300

    fix: images git pages

--- components/ShelfProduct.tsx
+++ components/ShelfProduct.tsx
@@ -50,7 +50,7 @@ const ShelfProduct = () => {
 
                 <div>
                     <div className="flex flex-col px-4 max-h-[156px] mb-4">
-                        <Image className="mx-auto rounded-sm" src="/site-brastemp.png" alt="Loja Brastemp" width={220} height={465} />
+                        <Image className="mx-auto rounded-sm" src="./site-brastemp.png" alt="Loja Brastemp" width={220} height={465} />
                     </div>
                     <div className="w-56 mx-auto mt-2 flex flex-col gap-4 justify-start">
                         <p className="text-black text-base font-medium">Loja Brastemp</p>
@@ -66,7 +66,7 @@ const ShelfProduct = () => {
 
                 <div>
                     <div className="flex flex-col px-4 max-h-[156px] mb-4">
-                        <Image className="mx-auto rounded-sm" src="/site-compracerta.png" alt="Loja Compra Certa" width={220} height={465} />
+                        <Image className="mx-auto rounded-sm" src="./site-compracerta.png" alt="Loja Compra Certa" width={220} height={465} />
                     </div>
                     <div className="w-56 mx-auto mt-2 flex flex-col gap-4 justify-start">
                         <p className="text-black text-base font-medium">Loja Compra Certa</p>
@@ -82,7 +82,7 @@ const ShelfProduct = () => {
 
                 <div>
                     <div className="flex flex-col px-4 max-h-[156px] mb-4">
-                        <Image className="mx-auto rounded-sm" src="/site-consul.png" alt="Loja Consul" width={220} height={465} />
+                        <Image className="mx-auto rounded-sm" src="./site-consul.png" alt="Loja Consul" width={220} height={465} />
                     </div>
                     <div className="w-56 mx-auto mt-2 flex flex-col gap-4 justify-start">
                         <p className="text-black text-base font-medium">Loja Consul</p>
@@ -98,7 +98,7 @@ const ShelfProduct = () => {
 
                 <div>
                     <div className="flex flex-col px-4 max-h-[156px] mb-4">
-                        <Image className="mx-auto rounded-sm" src="/site-thebar.png" alt="Loja The Bar" width={220} height={465} />
+                        <Image className="mx-auto rounded-sm" src="./site-thebar.png" alt="Loja The Bar" width={220} height={465} />
                     </div>
                     <div className="w-56 mx-auto mt-2 flex flex-col gap-4 justify-start">
                         <p className="text-black text-base font-medium">Loja The Bar</p>
